2ce2d5f6e81718ae58f91d6075719087b4c6e842
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-07-06 14:38:56 -0400

    Add "Hit Me" button, total score, and round fields.

--- TVBullsEye/ContentView.swift
+++ TVBullsEye/ContentView.swift
@@ -20,28 +20,46 @@ struct ContentView: View {
         VStack {
             Text("Guess the number:")
             TextField("1-100", text: $valueString, onEditingChanged: { _ in self.currentValue = Double(self.valueString) ?? 50.0
-            })
+            })  // TextField
                 .frame(width: 150.0)
             HStack {
                 Text("0")
                 GeometryReader { geometry in
                     ZStack {
-                        Rectangle()
+                        Rectangle()  // line
                             .frame(height: 8.0)
-                        Rectangle()
+                        Rectangle()  // marker
                             .frame(width: 8.0, height: 30.0)
                             .offset(x: geometry.size.width * (CGFloat(self.game.targetValue)/100.0 - 0.5), y: 0.0)
-                    }
-                }
+                    }  // ZStack
+                }  // Geometry Reader
                 Text("100")
+            }  // HStack
+                .padding(.horizontal)
+            Button(action: {
+                self.showAlert = true
+                self.game.checkGuess(Int(self.currentValue))
+            }) {
+                Text("Hit Me!")
             }
-            .padding(.horizontal)
-        }
-    }
-}
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
-}
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Your Score"), message: Text(String(game.scoreRound)),
+                      dismissButton: .default(Text("OK"), action: {
+                        self.game.startNewRound()
+                        self.valueString = ""
+                      }))
+            }  // .alert
+                .padding()
+            HStack {
+                Text("Total Score: \(game.scoreTotal)")
+                Text("Round: \(game.round)")
+            } // HStack
+        }  // VStack
+    }  // body
+}  // struct
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }  // previews
+}  // struct
